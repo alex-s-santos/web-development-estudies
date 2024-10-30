10984b2de6a20b0cf0c6a3f5227d6110dc075937
Alex Santos <alexsantos.alss.13@gmail.com>
2024-10-30 15:38:47 -0300

    inicia aula 8

---
+++ js/aula8-switchcase.js
@@ -0,0 +1,12 @@
+let form = document.querySelector('#form');
+let value = document.querySelector('#value');
+let installments = document.querySelector('#installments');
+let boxResult = document.querySelector('boxResult');
+let result = document.querySelector('result');
+
+const INTEREST_PERCENTAGE = 0.03;
+let interestValue;
+let options;
+
+
+console.log(installments);
