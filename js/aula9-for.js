f686d5059e6ebf684c2836b247e547bd09be259a
Alex Santos <alexsantos.alss.13@gmail.com>
2024-11-06 17:44:16 -0300

    finaliza aula 9 com resultado

--- js/aula9-for.js
+++ js/aula9-for.js
@@ -1,8 +1,9 @@
 let form = document.querySelector('#form');
 let numText = document.querySelector('#numText');
 let number = document.querySelector('#number');
+let result = document.querySelector('#result');
 
-
+result.style.display = 'none';
 form.addEventListener('submit', run);
 
 function run(e) {
@@ -15,6 +16,15 @@ function run(e) {
     numText.style.borderRadius = '5px'; 
     numText.style.padding = '0 2px';
 
-    
+    result.style.display = 'block';
+    result.style.width = '25%';
+    result.style.backgroundColor = '#1e88e5';
+    result.style.color = '#f2f2f2';
+    result.style.borderRadius = '3px';
+    result.style.height = '90px';
+
+    for(let i = 1; i <= 10; i++) {
+        result.innerHTML += `${number.value} x ${i} = ${number.value * i}<br>`;
+    }
 
 }
